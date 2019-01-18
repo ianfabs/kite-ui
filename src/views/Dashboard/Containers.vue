02bfa98dfcf933c29a6c39dd1c9ab001154347dd
Ian Fabs <ianfabs@outlook.com>
2019-01-17 20:11:10 -0500

    Added create container dialog and grid-layout to cards

--- src/views/Dashboard/Containers.vue
+++ src/views/Dashboard/Containers.vue
@@ -5,11 +5,9 @@
     v-for="container in containers" 
     :key="container.name"
     shadow="hover"
-    v-draggable
     >
       <div slot="header" class="el-card-header clearfix text-align-left">
         {{container.name}}
-        
         <div class="float-right">
           <el-tooltip effect="dark" content="Open file explorer" placement="bottom-end">
             <el-button class="pt-0 pb-0" type="text">
@@ -31,7 +29,7 @@
           width="200"
           trigger="hover"
           title="Status"
-          content="Running"
+          :content="container.status"
           >
           <span slot="reference" class="ml-1">
             <el-badge is-dot :type="container.status == 'running' ? 'success' : 'danger'">
@@ -45,11 +43,29 @@
       </div>
     </el-card>
     <!-- Create new card -->
-    <div class="container-card new" @click="handleClick">
+    <div class="container-card new" @click="dialogVisible = true">
       <div class="text-align-center">
         Create a new container
       </div>
     </div>
+    <el-dialog title="Shipping address" :visible.sync="dialogVisible">
+        <el-container>
+          <el-form :model="form">
+            <el-form-item label="Container Name" :label-width="'120px'">
+                <el-input placeholder="my-php" v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label="Container Image">
+                <el-select v-model="form.image" placeholder="Please select an image">
+                  <el-option v-for="image in images" :label="image.name" :value="image.name" :key="image.name"></el-option>
+                </el-select>
+            </el-form-item>
+          </el-form>
+        </el-container>
+        <span slot="footer" class="dialog-footer">
+            <el-button @click="dialogVisible = false">Cancel</el-button>
+            <el-button type="primary" @click="createContainer">Confirm</el-button>
+        </span>
+    </el-dialog>
   </el-container>
 </template>
 
@@ -63,25 +79,37 @@ export default {
       containers: [
         {
           name: "001416358php",
+          image: "sessymyer/mini-php",
           status: "running",
           created: (new Date()).toLocaleString()
+        }
+      ],
+      dialogVisible: false,
+      form: {},
+      images: [
+        {
+          name: "sessymyer/mini-php"
         }
       ]
     };
   },
   methods: {
     handleClick(){
-      alert("New Container Created");
+      
+    },
+    createContainer(event){
+      this.containers.push({name: this.form.name, status: "stopped", image: this.form.image, created: (new Date()).toLocaleString() });
+      this.dialogVisible = false;
     }
   },
   components: {
     FileUpload,
-    FileList
+    FileList,
   }
 };
 </script>
 
-<style>
+<style scoped>
 .container-card{
   width: 400px;
   height: 20vh;
@@ -95,5 +123,12 @@ export default {
   align-items: center;
   color: #909399;
   cursor: pointer;
+  margin-left: 1em;
+}
+.deck{
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 400px);
+  grid-row-gap: 3rem;
+  grid-column-gap: 2rem;
 }
 </style>
